29013470e6b26962ae1a9b2b1014060b5deb6e56
ekesas <ekesas@gmail.com>
2014-06-09 08:22:24 -0500

    Added a comment here too

---
+++ words.rb
@@ -0,0 +1,23 @@
+words = []
+
+while true
+
+puts "please type in a word"
+
+words << gets.chomp
+
+if words == ""
+
+	break 
+
+end 	
+
+end 
+
+# I am adding a comment here for git purposes. 
+
+
+
+
+
+
