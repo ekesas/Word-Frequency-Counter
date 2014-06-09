ac8a80914a4e8f590c033fe1193fe073fd087c9f
ekesas <ekesas@gmail.com>
2014-06-09 08:21:02 -0500

    Added a second comment

--- angryboss.rb
+++ angryboss.rb
@@ -5,4 +5,4 @@ response = gets.chomp
 puts "WHAT THE HELL DO YOU MEAN " + "'" +response.upcase + "'" + "??! YOU ARE FIRED!!"
 
 # I am using this as a practice thing for me" 
-
+#I am gonna add anothor comment here just for the hell of it"
